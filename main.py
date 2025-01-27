392b034a1565ac68adb37cd375b040138929dbd9
ChuffedDom <98715130+ChuffedDom@users.noreply.github.com>
2025-01-27 20:16:35 +0000

    Added more CORS stuff

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ import requests
 from flask_cors import CORS
 
 app = flask.Flask(__name__)
-cors = CORS(app, resources={r"/proxy": {"origins": "*"}})
+CORS(app, supports_credentials=True, resources={r"/*": {"origins": "http://localhost:5500"}})
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "http://localhost:5500"  # Match the client origin
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    response.headers["Access-Control-Allow-Credentials"] = "true"  # Explicitly allow credentials
+    return response
 
 @app.route('/', methods=['GET'])
 def health_check():
