45083e11799a514b534edbeb56caad2a0ff11db9
ChuffedDom <98715130+ChuffedDom@users.noreply.github.com>
2025-01-27 22:52:11 +0000

    Roll back and trying again with cors

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
+import logging
 import flask
 import requests
 from flask_cors import CORS
 
 app = flask.Flask(__name__)
 CORS(app, supports_credentials=True, resources={r"/*": {"origins": "http://localhost:5500"}})
-
-@app.after_request
-def add_cors_headers(response):
-    response.headers["Access-Control-Allow-Origin"] = "http://localhost:5500"  # Match the client origin
-    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
-    response.headers["Access-Control-Allow-Credentials"] = "true"  # Explicitly allow credentials
-    return response
 
 @app.route('/', methods=['GET'])
 def health_check():
@@ -28,14 +21,6 @@ def mirror():
 
 @app.route('/proxy', methods=['POST', 'OPTIONS'])
 def proxy():
-    if request.method == 'OPTIONS':
-        # Respond to preflight request
-        response = app.response_class()
-        response.headers["Access-Control-Allow-Origin"] = "http://localhost:5500"
-        response.headers["Access-Control-Allow-Methods"] = "POST, OPTIONS"
-        response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
-        response.headers["Access-Control-Allow-Credentials"] = "true"
-        return response, 204
     
     request = flask.request.json
     target = request.get('target')
